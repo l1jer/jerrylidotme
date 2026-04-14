6e580845a5406ef7d49a850457693da7730854ec
Jerrylidotme <42858482+l1jer@users.noreply.github.com>
2026-04-15 01:29:44 +1000

    Fix footer link URL for Digolas Forge in Footer component to ensure correct navigation.

--- ArchiveVersions/ver1.0_dec_2024/src/components/Footer.tsx
+++ ArchiveVersions/ver1.0_dec_2024/src/components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
         <div className="flex flex-col items-center">
           <div className="w-16 h-1 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-full mb-6"></div>
           <p className="text-gray-300  text-center">
-            © {new Date().getFullYear()} Jerry Li,  <a className=" text-gray-300 hover:text-indigo-300" href="https://digolas-forge.com" target="_blank" rel="noopener noreferrer">@Digolas Forge</a>. All rights reserved.
+            © {new Date().getFullYear()} Jerry Li,  <a className=" text-gray-300 hover:text-indigo-300" href="https://digolasforge.com" target="_blank" rel="noopener noreferrer">@Digolas Forge</a>. All rights reserved.
           </p>
           <p className="text-gray-600 text-xs mt-2">
             Crafted with passion and creativity
